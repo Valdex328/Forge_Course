65d0435796982df80f36c8a92d9c775b5b7089d9
Valdex328 <valderrama.daniel@correounivalle.edu.co>
2025-08-14 17:37:40 -0500

    Files for import library training uploaded

--- src/ImportERC20.sol
+++ src/ImportERC20.sol
@@ -1,9 +1,20 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
+/*
+USING FORGE INSTALL <LIBRARY>
 import "lib/solmate/src/tokens/ERC20.sol";
 
 contract Token is ERC20("CoinTest", "CTE", 18) {
-    
+
+} */
+
+/*
+USING NPM I <LIBRARY>
+*/
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOwnable is Ownable {
+    constructor() Ownable(msg.sender) {}
 }
 
